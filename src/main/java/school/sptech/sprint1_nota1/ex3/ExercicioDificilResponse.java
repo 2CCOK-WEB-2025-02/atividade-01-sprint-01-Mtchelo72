951b37f95917498db95de47dd4587139a2670894
Mtchelo72 <marcelo.silvajr@sptech.school>
2025-08-20 14:06:26 -0300

    Java part. 3 response (falta a logica do desafio)

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
@@ -1,5 +1,27 @@
 package school.sptech.sprint1_nota1.ex3;
 
 public class ExercicioDificilResponse {
+    public Integer soma;
+    public Integer enesimoTermo;
 
+    public ExercicioDificilResponse(Integer soma, Integer enesimoTermo) {
+        this.soma = soma;
+        this.enesimoTermo = enesimoTermo;
+    }
+
+    public Integer getSoma() {
+        return soma;
+    }
+
+    public void setSoma(Integer soma) {
+        this.soma = soma;
+    }
+
+    public Integer getEnesimoTermo() {
+        return enesimoTermo;
+    }
+
+    public void setEnesimoTermo(Integer enesimoTermo) {
+        this.enesimoTermo = enesimoTermo;
+    }
 }
